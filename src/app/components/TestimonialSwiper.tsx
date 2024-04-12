079822e54f1c1c78af8f4613e4c5a30b85cf57d4
spbovarnick <spbovarnick@gmail.com>
2024-04-12 10:09:28 -0700

    swiper behaving as desired in mobile

--- src/app/components/TestimonialSwiper.tsx
+++ src/app/components/TestimonialSwiper.tsx
@@ -2,49 +2,45 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
-// import { SwiperOptions } from "swiper/types";
-import 'swiper/css'
+import 'swiper/css';
+import "swiper/css/pagination"
 import { TestimonialQueryResponse } from "../lib/types";
-
-// const swiperParams: SwiperOptions = {
-//   direction: "horizontal",
-//   loop: false,
-//   pagination: {
-//     el: '.swiper-pagination'
-//   },
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-//   scrollbar: {
-//     el: ".swiper-scrollbar",
-//   },
-// }
+import "../styles/swiper.css";
 
 interface TestimonialSwiperProps {
   testimonials: TestimonialQueryResponse[],
-}
-
-// const swiper = new Swiper('.swiper', swiperParams)
+};
 
 const TestimonialSwiper: React.FC<TestimonialSwiperProps>  = ({ testimonials }) => {
-  console.log(testimonials)
 
   return (
-    <>
+    <div className="px-5">
       <Swiper
+        slidesPerView={1.5}
+        spaceBetween={12}
         modules={[Pagination]}
-        // pagination={{
-        //   dynamicBullets: true,
-        // }}
+        pagination={{
+          dynamicBullets: true,
+          clickable: true,
+        }}
+        slidesOffsetAfter={32}
       >
         {testimonials?.map((item) => (
-          <SwiperSlide key={item._id}>
-            {item.testimonial}
+          <SwiperSlide
+            key={item._id}
+            className="text-base"
+          >
+            <p>
+              {`"${item.testimonial.trimEnd().trimStart()}"`}
+            </p>
+            <br />
+            <p>
+              {item.client_name}
+            </p>
           </SwiperSlide>
         ))}
       </Swiper>
-    </>
+    </div>
   )
 }
 
