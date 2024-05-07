ac1b65238a7dcb7baafefc8c0ec333822b226911
spbovarnick <spbovarnick@gmail.com>
2024-05-06 18:57:31 -0700

    icons saved

--- src/app/components/Icons.tsx
+++ src/app/components/Icons.tsx
@@ -1,5 +1,5 @@
 export const Cross = (props: any) => (
-  <svg id="x" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 15 15" {...props}>
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 15 15" {...props}>
     <path fill="currentColor" fillRule="evenodd" d="M12.854 2.854a.5.5 0 0 0-.708-.708L7.5 6.793L2.854 2.146a.5.5 0 1 0-.708.708L6.793 7.5l-4.647 4.646a.5.5 0 0 0 .708.708L7.5 8.207l4.646 4.647a.5.5 0 0 0 .708-.708L8.207 7.5l4.647-4.646Z" clipRule="evenodd"></path>
   </svg>
 )
