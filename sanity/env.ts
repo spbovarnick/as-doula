219701c0edde0fa32a025139882e974d3ee41615
spbovarnick <spbovarnick@gmail.com>
2024-08-22 17:42:57 -0700

    env.ts fat trimmed

--- sanity/env.ts
+++ sanity/env.ts
@@ -16,12 +16,6 @@ const getDataset = () => {
 };
 
 export const dataset = getDataset();
-// export const dataset = assertValue(process.env.NEXT_PUBLIC_SANITY_PROD_DATASET as string, "Missing environment variable: NEXT_PUBLIC_SANITY_PROD_DATASET");
-
-// export const dataset = process.env.NODE_ENV === "development" ? assertValue(
-//   process.env.NEXT_PUBLIC_SANITY_DEV_DATASET,
-//   'Missing environment variable: NEXT_PUBLIC_SANITY_DEV_DATASET'
-// ) : assertValue(process.env.NEXT_PUBLIC_SANITY_PROD_DATASET, 'Missing environment variable: NEXT_PUBLIC_SANITY_PROD_DATASET');
 
 export const projectId = assertValue(
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
