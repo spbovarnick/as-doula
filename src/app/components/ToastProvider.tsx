b7c453993a05073ac41a0278db24fc0fd3921b37
spbovarnick <spbovarnick@gmail.com>
2025-02-04 15:28:48 -0800

    sticky footer, toast div height fixed

--- src/app/components/ToastProvider.tsx
+++ src/app/components/ToastProvider.tsx
@@ -16,6 +16,7 @@ export default function ToastProvider({
         containerClassName="poop"
         containerStyle={{
           position: "absolute",
+          height: "fit-content"
         }} />
     </>
   )
