49d613f02d5e897d0efc404b56e20b6951c9be25
spbovarnick <spbovarnick@gmail.com>
2025-02-05 16:25:16 -0800

    suspense added to landing page'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import CtaButton from "./components/CtaButton";
 import TestimonialSwiper from "./components/TestimonialSwiper";
 import ClientImage from "./components/ClientImg";
 import landing from './landing.module.css';
+import { Suspense } from "react";
 
 
 export default async function Home() {
@@ -24,7 +25,7 @@ export default async function Home() {
 
   return (
     <div >
-
+      <Suspense>
       <div className="mb-5 sm:grid sm:grid-cols-3 sm:relative sm:gap-x-4 sm:mb-8">
         <div className="mb-8 sm:flex sm:flex-col sm:justify-center sm:col-span-1 ">
           {/* {landingBlurb.headshot && */}
@@ -55,7 +56,7 @@ export default async function Home() {
         </div>
       </div>
 
-
+      </Suspense>
       <TestimonialSwiper testimonials={testimonials} />
     </div>
   );
