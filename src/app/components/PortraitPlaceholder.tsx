f0a0e35726baa670147ccd6fa087d6ff282a3903
spbovarnick <spbovarnick@gmail.com>
2025-02-05 20:35:27 -0800

    fixed placeholder path

--- src/app/components/PortraitPlaceholder.tsx
+++ src/app/components/PortraitPlaceholder.tsx
@@ -1,10 +1,9 @@
-import "../../../public/portrait1.png"
 import Image from "next/image"
 
 export default function PortraitPlaceholder(){
   return (
     <Image
-      src={"/../../../public/portrait1.png"}
+      src={"/portrait1.png"}
       height={1024}
       width={1024}
       alt="placeholder"
