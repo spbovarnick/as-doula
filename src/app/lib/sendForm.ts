8f772bf34ff34b7d1eb89628bda3498f4d52f8e0
spbovarnick <spbovarnick@gmail.com>
2025-05-20 19:28:18 -0700

    removed host and port

--- src/app/lib/sendForm.ts
+++ src/app/lib/sendForm.ts
@@ -5,8 +5,6 @@ import { FormValues } from "@/app/lib/types";
 import nodemailer from 'nodemailer';
 const transporter = nodemailer.createTransport({
   service: "gmail",
-  host: "smtp.gmail.com",
-  port: 465,
   secure: true,
   auth: {
     user: process.env.EMAIL,
